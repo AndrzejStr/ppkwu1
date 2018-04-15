e977146bcd739aa28aaef9604681dbd01cb886c4
TotallyUniqueUsername <andrzejstrek@o2.pl>
2018-04-15 10:03:02 +0200

    poczatkowy commit - postawienie springboot

---
+++ src/main/java/zadanie1/reverse/ReverseApplication.java
@@ -0,0 +1,12 @@
+package zadanie1.reverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseApplication.class, args);
+	}
+}
